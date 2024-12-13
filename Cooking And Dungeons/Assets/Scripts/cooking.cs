a823ca1de9081d3ecc2157c6987fb89d5115f9fd
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2024-12-13 16:44:19 -0500

    cooking almost work

--- Cooking And Dungeons/Assets/Scripts/cooking.cs
+++ Cooking And Dungeons/Assets/Scripts/cooking.cs
@@ -1,5 +1,9 @@
 
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static recipeReader;
 
 public class cooking : MonoBehaviour
 {
@@ -7,12 +11,39 @@ public class cooking : MonoBehaviour
 
     //filled using the script inventory
     public System.Collections.Generic.List<GameObject> tilesArr;
+    public recipeReader recipeReaderScript;
+    public void cook()
+    {
 
+        List<Recipe> recipeList = recipeReaderScript.RecipeList;
 
+        foreach (Recipe recipe in recipeList)
+        {
+            Debug.Log(recipe.name);
+            foreach(string ingredientNeeded in recipe.ingredients)
+            {
+                int goodIngredient = 0;
+                foreach(GameObject ingredientInTiles in tilesArr)
+                {
+                    if(ingredientInTiles.GetComponent<itemIdentity>().itemName == ingredientNeeded)
+                    {
+                        goodIngredient++;
+                    }
+                }
+                if(goodIngredient == recipe.ingredients.Count)
+                {
+                    Debug.Log(recipe.name + "cuisiner");
+                }
+            }
+        }
+
+        Debug.Log(recipeList.Count);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //cook();
+
     }
 
     // Update is called once per frame
--- Cooking And Dungeons/Assets/Scripts/recipeReader.cs
+++ Cooking And Dungeons/Assets/Scripts/recipeReader.cs
@@ -1,35 +1,24 @@
+using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Serialization;
 using UnityEngine;
 
 public class recipeReader : MonoBehaviour
 {
-    public TextAsset recipesJSON;
 
-
-
-    // Must be the same as the JSON
     [System.Serializable]
     public class Recipe
     {
         public string name;
-        public string[] ingredients;
+        public List<string> ingredients;
         public int lvl;
+
     }
 
+    public List<Recipe> RecipeList = new();
 
-    //Must be the same as the JSON
-    [System.Serializable]
-    public class RecipeList
-    {
-        public Recipe[] recipes;
-    }
-
-    public RecipeList myRecipeList = new RecipeList();
-  
 
     void Start()
     {
-        myRecipeList = JsonUtility.FromJson<RecipeList>(recipesJSON.text);
     }
   
     
